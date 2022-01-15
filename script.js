078d3ec71a0ec7f8f85fc97d2e73f1f73ba860fe
BuildTools <unconfigured@null.spigotmc.org>
2022-01-14 18:52:38 -0800

    stopped keyboarding started mousing

--- script.js
+++ script.js
@@ -9,5 +9,19 @@ window.addEventListener('keydown', function(event) {
     xhr.send(params);
 });
 */
-document.requestPointerLock();
+var canvas = document.getElementbyID("canvas_elem").requestPointerLock();
+canvas.onclick = function () {
+    canvas.requestPointerLock();
+}
+//canvas.requestPointerLock();
+document.addEventListener("pointerlockchange", pointerlockchanged, true);
+function pointerlockchanged() {
+    document.addEventListener("mousemove", updatePos, falsr);
+}
+function updatePos(h) {
+    var xhr = new XMLHttpRequest();
+    var url = window.location.href;
+    var params = `x=${h.movementX}&y=${h.movementY}`;
+    xhr.send(params);
+}
 })();
